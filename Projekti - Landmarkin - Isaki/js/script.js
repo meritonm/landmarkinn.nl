2cb7af8b050a2e492bd655e87ba2608b314ed199
meritonm <108494516+meritonm@users.noreply.github.com>
2022-08-18 00:55:24 +0200

    Add files via upload

---
+++ Projekti - Landmarkin - Isaki/js/script.js
@@ -0,0 +1,156 @@
+"use strict";
+// fixing changing year automaticly
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
+
+///////////////////////////////////
+//Make mobile navigation work
+// const btnNav = document.querySelector(".btn-mobile-nav");
+// const headerEl = document.querySelector(".header");
+
+// btnNavEl.addEventListener("click", function() {
+//     headerEl.classList.toggle("nav-open");
+// });
+
+// (".nav - open");
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function() {
+    headerEl.classList.toggle("nav-open");
+});
+
+/////////////////////////////////////////////////////////////
+// Smoth scrolling annimation
+// sft
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+        // scroll back to top
+        if (href === "#")
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        // scroll to other link
+        if (href !== "#" && href.startsWith("#")) {
+            const sectionEl = document.querySelector(href);
+            sectionEl.scrollIntoView({ behavior: "smooth" });
+        }
+        if (href)
+            if (link.classList.contains("main-nav-link"))
+            // close mobile navigation
+                headerEl.classList.toggle("nav-open");
+    });
+});
+
+/////////////////////////////////////////////////////////////
+// //  Stick nav
+// // const sectionWhoEl = document.querySelector(".section-who");
+
+// const obs = new IntersectionObserver(
+//     function(entries) {
+//         const ent = entries[0];
+//         if (ent.isIntersecting === false) {
+//             document.body.classList.add("sticky");
+//         }
+
+//         if (ent.isIntersecting === true) {
+//             document.body.classList.remove("sticky");
+//         }
+//     }, {
+//         root: null,
+//         threshold: 0,
+//         rootMargin: "-80px",
+//     }
+// );
+// obs.observe(sectionWhoEl);
+
+/////////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+//////////////////////////////////////////////////////////////
+// give social icons function
+let list = document.querySelectorAll(".sci li");
+let bg = document.querySelector(".socialMedia");
+list.forEach((elements) => {
+    elements.addEventListener("mouseenter", function(event) {
+        let newpage = event.target.toggle(".footer-link");
+    });
+});
+
+//////////////////////
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+/*
+.no-flexbox-gap .main-nav-list li:not(:last-child) {
+  margin-right: 4.8rem;
+}
+
+.no-flexbox-gap .list-item:not(:last-child) {
+  margin-bottom: 1.6rem;
+}
+
+.no-flexbox-gap .list-icon:not(:last-child) {
+  margin-right: 1.6rem;
+}
+
+
+.no-flexbox-gap .---:not(:last-child) {
+  margin-bottom: 2rem;
+}
+
+.no-flexbox-gap  {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .footer-row div:not(:last-child) {
+  margin-right: 6.4rem;
+}
+
+.no-flexbox-gap .social-links li:not(:last-child) {
+  margin-right: 2.4rem;
+}
+
+.no-flexbox-gap .footer-nav li:not(:last-child) {
+  margin-bottom: 2.4rem;
+}
+
+@media (max-width: 75em) {
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 3.2rem;
+  }
+}
+
+@media (max-width: 59em) {
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 0;
+    margin-bottom: 4.8rem;
+  }
+}
+*/
